ab9c86c02b27afe10e630755ed893c14259bfa22
ishajain15 <jainisha2001@yahoo.com>
2017-08-13 19:36:48 -0700

    App

---
+++ app/src/main/java/com/example/gwc/spiltmate/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.gwc.spiltmate;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    transaction.replace(R.id.content,new HomeFragment()).commit();
+                    return true;
+                case R.id.navigation_share:
+                    transaction.replace(R.id.content,new ShareFragment()).commit();
+                    return true;
+                case R.id.navigation_create:
+                    transaction.replace(R.id.content,new CreateFragment()).commit();
+                    return true;
+                case R.id.navigation_calendar:
+                    transaction.replace(R.id.content,new CalendarFragment()).commit();
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content,new HomeFragment()).commit();
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+    }
+
+}
